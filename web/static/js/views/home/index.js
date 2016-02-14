3d39a299402387c81b5ad1605746ca59f686fda7
Ricardo <bigardone@gmail.com>
2016-02-14 19:14:59 +0100

    Group time entries by updated at date

--- web/static/js/views/home/index.js
+++ web/static/js/views/home/index.js
@@ -1,12 +1,12 @@
-import React                from 'react';
-import { connect }          from 'react-redux';
-import classnames           from 'classnames';
-import moment               from 'moment';
-
-import { setDocumentTitle } from '../../utils';
-import Timer                from '../../components/timer';
-import {fetchTimeEntries}   from '../../actions/time_entries';
-import TimeEntryItem        from '../../components/time_entries/item';
+import React                      from 'react';
+import { connect }                from 'react-redux';
+import classnames                 from 'classnames';
+import moment                     from 'moment';
+import { setDocumentTitle }       from '../../utils';
+import Timer                      from '../../components/timer';
+import {fetchTimeEntries}         from '../../actions/time_entries';
+import TimeEntryItem              from '../../components/time_entries/item';
+import { timexDateToString }  from '../../utils';
 
 class HomeIndexView extends React.Component {
   componentDidMount() {
@@ -19,23 +19,57 @@ class HomeIndexView extends React.Component {
   _renderItems() {
     const { items } = this.props;
 
-    const itemsNodes = items.map((item) => {
+    const groups = this._buildItemGroups(items);
+
+    return Object.keys(groups).map((date) => {
+      const items = groups[date];
+
+      return (
+        <section key={date} className="time-entries">
+          <header>
+            <span className="title">{::this._headerText(date)}</span>
+            <small>{::this._renderTotalTime(items)}</small>
+          </header>
+          <ul>
+            {::this._groupItemNodes(items)}
+          </ul>
+        </section>
+      );
+    });
+  }
+
+  _headerText(dateKey) {
+    const date = moment(new Date(dateKey));
+
+    switch (moment().diff(date, 'days')) {
+      case 0:
+        return 'Today';
+      case 1:
+        return 'Yesterday';
+      default:
+        return date.format('ddd, DD MMM');
+    }
+  }
+
+  _groupItemNodes(items) {
+    return items.map((item) => {
       return (
         <TimeEntryItem key={item.id} {...item} />
       );
     });
+  }
 
-    return (
-      <section className="time-entries">
-        <header>
-          <span className="title">Time entries</span>
-          <small>{::this._renderTotalTime(items)}</small>
-        </header>
-        <ul>
-          {itemsNodes}
-        </ul>
-      </section>
-    );
+  _buildItemGroups(items) {
+    const groups = {};
+
+    items.forEach((item) => {
+      const key = timexDateToString(item.updated_at);
+
+      groups[key] = groups[key] || [];
+      groups[key].push(item);
+    });
+
+    return groups;
   }
 
   _renderTotalTime(items) {
