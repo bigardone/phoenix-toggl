c616b4bfbbfe898e6ab8885082e3499978537736
Ricardo García Vega <bigardone@gmail.com>
2016-02-20 13:29:29 +0100

    Reports index view

---
+++ web/static/js/views/reports/index.js
@@ -0,0 +1,45 @@
+import React, {PropTypes} from 'react';
+import { connect }        from 'react-redux';
+import { fetchData }      from '../../actions/reports';
+import ReportContainer    from '../../components/reports/container';
+
+class ReportsIndeView extends React.Component {
+  componentDidMount() {
+    const { dispatch, channel } = this.props;
+
+    if (channel == null) return false;
+
+    dispatch(fetchData(channel));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { dispatch, channel } = this.props;
+    const nextChannel = nextProps.channel;
+
+    if (channel == null && nextChannel != null) dispatch(fetchData(nextChannel));
+  }
+
+  render() {
+    const { fetching, data } = this.props;
+
+    return (
+      <div id="reports_index" className="view-container">
+        <div className="container">
+          <header className="view-header">
+            <h1>Summary report</h1>
+            <div className="range-selector">
+              <h2>This week</h2>
+            </div>
+          </header>
+          <ReportContainer data={data} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  { ...state.reports, channel: state.session.channel }
+);
+
+export default connect(mapStateToProps)(ReportsIndeView);
